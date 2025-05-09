b3e7269b475a4d9b5088cdf6dbb909f02ca31cd8
Matt Jaquiery <matt.jaquiery@dtc.ox.ac.uk>
2025-05-09 16:59:27 +0100

    fix: remove unused import

--- src/components/steps/ResolveDuplicatesStep.tsx
+++ src/components/steps/ResolveDuplicatesStep.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import {useEffect, useState} from 'react';
+import {useEffect} from 'react';
 import { Button } from '@/components/ui/button';
 import StepPanel from '@/components/steps/StepPanel';
 import {useInputData} from "@/lib/InputDataContext";
