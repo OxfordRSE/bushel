e6c3bf6f90c27a9e6c917a4ed788d1b3d876221c
Matt Jaquiery <matt.jaquiery@dtc.ox.ac.uk>
2025-05-09 10:31:13 +0100

    dbg: no minification

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  compress: false,
+  swcMinify: false, // disable SWC minification
 };
 
 export default nextConfig;
